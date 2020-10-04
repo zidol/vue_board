c41224b14d4a48d22fb0bd36c89d7801e75268e8
zidol <ajh3166@naver.com>
2020-10-04 18:36:47 +0900

    add list item comp and loading spinner

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,39 @@
 import axios from 'axios';
-import store from '@/store/index';
+// import store from '@/store/index';
+import { setInterceptors } from './common/interceptors';
 
-// axios 공통 설정
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL, //.env파일에 설정해 놓은 url
-  headers: {
-    //헤더안에 기본 적으로 http의 header에 있는 속성 중 하나
-    Authorization: store.state.token,
-  },
-});
+// 액시오스 초기화 함수
+//함수의 결과가 interceptor가 세팅 된 instace가 생성됨(return) : interceptor 모듈화
+function createInstance() {
+  // axios 공통 설정
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL, //.env파일에 설정해 놓은 url
+    // headers: {
+    //   //헤더안에 기본 적으로 http의 header에 있는 속성 중 하나
+    //  // 자바스크립트 레벨에선 토큰값이 바뀌어도 기존의 값이 들어가기 때문에 login api가 실행되도 빈 토큰값이 들어감.
+    // //해결책: 인터셉터를 이용해 요청 보내기전에 headers에 넣음
+    //   Authorization: store.state.token,
+    // },
+  });
+  return setInterceptors(instance); //interceptor 세팅
+}
 
+const instance = createInstance();
+
+//회원가입 api
 function registerUser(userData) {
   //   const url = 'http://localhost:3000/signup';
   //   return axios.post(url, userData);
   return instance.post('signup', userData);
 }
-
+//로그인 api
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+//게시글 조회 api
+function fectchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fectchPosts };
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,52 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
+        <!-- <li v-for="postItem in postItems" :key="postItem._id">
+          <div class="post-title">{{ postItem.title }}</div>
+          <div class="post-contents">{{ postItem.contents }}</div>
+          <div class="post-time">{{ postItem.createdAt }}</div>
+        </li> -->
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fectchPosts } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      // response.data == {data}
+      this.isLoading = true;
+      const { data } = await fectchPosts();
+      console.log(data.posts);
+      this.isLoading = false;
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
