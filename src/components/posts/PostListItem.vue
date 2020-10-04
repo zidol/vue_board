df4581eaa4c6bedc42b0f5413e1fd85de1013c1f
zidol <ajh3166@naver.com>
2020-10-04 23:50:24 +0900

    add delete Logic

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -1,17 +1,35 @@
 <template>
   <li>
-    <div class="post-title">{{ postItem.title }}</div>
-    <div class="post-contents">{{ postItem.contents }}</div>
-    <div class="post-time">{{ postItem.createdAt }}</div>
+    <div class="post-title">
+      {{ postItem.title }}
+    </div>
+    <div class="post-contents">
+      {{ postItem.contents }}
+    </div>
+    <div class="post-time">
+      {{ postItem.createdAt }}
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
+    </div>
   </li>
 </template>
 
 <script>
+import { deletePost } from '@/api/posts';
 export default {
   props: {
     postItem: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    async deleteItem() {
+      if (confirm('삭제 하시겠습니가?')) {
+        await deletePost(this.postItem._id);
+        this.$emit('refresh');
+      }
+      // console.log('deleted!!');
     },
   },
 };
